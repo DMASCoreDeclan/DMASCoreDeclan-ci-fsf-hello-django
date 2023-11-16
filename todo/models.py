483f1d4d84427ed9f2bd20bc3c588d48a937b027
Declan Sweeney <declan.sweeney@gmail.com>
2023-11-16 13:50:39 +0000

    Update todo_list.html with code to iterate through item table

--- todo/models.py
+++ todo/models.py
@@ -1,4 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
-# class Item():
+
+'''
+Create Table Item with 2 fields: name and done
+'''
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    '''
+    Returns the NAME of the object instead of the Type of object as defined
+    in line 590 of
+    https://github.com/django/django/blob/main/django/db/models/base.py
+    ''' 
+    def __str__(self):
+        return self.name
